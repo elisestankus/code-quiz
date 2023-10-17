b9a5f03c74a661cbe90d355e1f6baeaefce5071a
estan <elise.stankus@gmail.com>
2023-10-16 23:14:52 -0700

    declared questions vars

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,8 @@
 
 var intro = document.querySelector("#intro")
+var Q1 = document.querySelector("#Q1")
+var Q2 = document.querySelector("#Q2")
+var Q3 = document.querySelector("#Q3")
+var Q4 = document.querySelector("#Q4")
+var Q5 = document.querySelector("#Q5")
 var endPage = document.querySelector("#endPage")
